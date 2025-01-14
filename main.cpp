37e3f1ce367d4fc1462fe3a7daff197c7a25b368
StarryPurple <starrypurple@163.com>
2025-01-15 01:06:24 +0800

    update: Initially implemented priority_queue (using pairing heap). Note: Allocate / Deallocate on heap as late as possible, for you won't know if there's an exception that interrupts the process, leaving something lost before they're handled.

--- main.cpp
+++ main.cpp
@@ -1,34 +1,121 @@
-#include "vector/src/vector.hpp"
-// #include "priority_queue/src/priority_queue.hpp"
+// #include "vector/src/vector.hpp"
+#include "priority_queue/src/priority_queue.hpp"
 // #include "map/src/map.hpp"
 
-#include "vector/data/class-integer.hpp"
-#include "vector/data/class-matrix.hpp"
-#include "vector/data/class-bint.hpp"
+#include <iostream>
+#include <queue>
+#include <ctime>
+#include <cstdlib>
 
-
-void complex_test()
+void TestConstructorAndPush()
 {
-    std::cout << "Supplementary test for large amounts of data ..." << std::endl;
-    sjtu::vector<Diamond::Matrix<Util::Bint>> myVec;
-    for (int i = 1; i <= 1926; ++i)
-        myVec.push_back(Diamond::Matrix<Util::Bint>(i % 8 + 1, i % 17 + 1, Util::Bint(i * 817)));
-    int o = 1234;
-    while (o--)
-        myVec.pop_back();
-    myVec = myVec;
-    int _ = 20, __ = myVec.size();
-    while (_--)
-    {
-        if (_ % 2 == 0)
-            std::cout << myVec[_][0][0] << std::endl;
-        else
-            std::cout << myVec[__ - _][0][0] << std::endl;
-    }
-    std::cout << "Finished!" << std::endl;
+	std::cout << "Testing constructors, destructor and push..." << std::endl;
+	sjtu::priority_queue<int> pq;
+	for (int i = 100; i > 0; --i) {
+		pq.push(i);
+	}
+	while (!pq.empty()) {
+		std::cout << pq.top() << " ";
+		pq.pop();
+	}
+	std::cout << std::endl;
+	for (int i = 1000; i > 100; --i) {
+		pq.push(i);
+	}
+	sjtu::priority_queue<int> pqBack(pq);
+	std::cout << pqBack.size() << std::endl;
+	sjtu::priority_queue<int> pqBB;
+	for (int i = 10; i <= 10000; ++i) {
+		pqBB.push(i);
+	}
+	std::cout << pqBB.size() << std::endl;
+	pqBB = pq;
+	std::cout << pqBB.size() << std::endl;
 }
 
-int main()
+void TestSize()
 {
-    complex_test();
+	std::cout << "Testing size()" << std::endl;
+	sjtu::priority_queue<long long> pq;
+	for (int i = 1; i <= 1000; ++i) {
+		pq.push(rand());
+	}
+	std::cout << pq.size() << std::endl;
+}
+
+void TestException()
+{
+	sjtu::priority_queue<int> pq;
+	try {
+		std::cout << pq.top() << std::endl;
+	} catch (...) {
+		std::cout << "Throw correctly." << std::endl;
+	}
+}
+
+struct Natural {
+    int x;
+
+    Natural(int _x = 0) { x = _x; }
+
+    friend bool operator<(const Natural &lhs, const Natural &rhs) {
+        if (lhs.x < 0 || rhs.x < 0)
+            throw sjtu::runtime_error();
+        return lhs.x < rhs.x;
+    }
+};
+
+void TestCompareException() {
+	std::cout << "Testing compare exception...";
+
+	sjtu::priority_queue<Natural> pq;
+    static int dat[2000], ans[2000];
+	int pos = 0;
+    for (int i = 1; i <= 1000; ++i) {
+        dat[i] = i;
+		if (rand() % 10 == 0)
+			dat[i] = -i;
+		else ans[++pos] = i;
+	}
+
+    for (int i = 1000; i > 1; --i) {
+        unsigned int x = rand();
+        int p = x % (i - 1) + 1;
+        std::swap(dat[i], dat[p]);
+    }
+
+    while (dat[1] < 0) {
+        unsigned int x = rand();
+        int p = x % 1000 + 1;
+        std::swap(dat[1], dat[p]);
+    }
+
+
+    for (int i = 1; i <= 1000; ++i) {
+        try {
+            pq.push(Natural(dat[i]));
+        } catch (sjtu::runtime_error) {
+            if (dat[i] >= 0)
+                return std::cout << std::endl, void();
+        }
+    }
+
+    for (int i = pos; i; --i) {
+        Natural t = pq.top();
+        if (t.x != ans[i])
+                return std::cout << std::endl, void();
+        pq.pop();
+    }
+
+    std::cout << "ok." << std::endl;
+
+}
+
+int main(int argc, char *const argv[])
+{
+	TestConstructorAndPush();
+	TestSize();
+	TestException();
+    TestCompareException();
+	return 0;
 }
--- priority_queue/src/priority_queue.hpp
+++ priority_queue/src/priority_queue.hpp
@@ -12,63 +12,174 @@ namespace sjtu {
  */
 template<typename T, class Compare = std::less<T>>
 class priority_queue {
+private:
+  struct Node {
+    T *val_ptr = nullptr;
+    Node *child = nullptr, *sibling = nullptr;
+  };
+  Node *root_;
+  size_t size_;
+  Compare comparer_;
+
+  void copy_heap(Node *des, Node *src) {
+    if(src->child == nullptr) return;
+    des->child = new Node;
+    des->child->val_ptr = new T(*src->child->val_ptr);
+    copy_heap(des->child, src->child);
+    Node *cur_src = src->child, *cur_des = des->child;
+    while(cur_src->sibling != nullptr) {
+      cur_des->sibling = new Node;
+      cur_des->sibling->val_ptr = new T(*cur_src->sibling->val_ptr);
+      copy_heap(cur_des->sibling, cur_src->sibling);
+      cur_src = cur_src->sibling;
+      cur_des = cur_des->sibling;
+    }
+  }
+  void free_heap(Node *ptr) {
+    if(ptr == nullptr) return;
+    Node *cur = ptr->child, *del;
+    delete ptr->val_ptr;
+    delete ptr;
+    while(cur != nullptr) {
+      del = cur; cur = cur->sibling;
+      free_heap(del);
+    }
+  }
+  Node* multiple_merge(Node *cur) {
+    if(cur == nullptr || cur->sibling == nullptr) return cur;
+    Node *nxt = cur->sibling, *rest = multiple_merge(nxt->sibling);
+    cur->sibling = nullptr;
+    nxt->sibling = nullptr;
+    if(comparer_(*cur->val_ptr, *nxt->val_ptr)) {
+      cur->sibling = nxt->child;
+      nxt->child = cur;
+      cur = nxt;
+    } else {
+      nxt->sibling = cur->child;
+      cur->child = nxt;
+    }
+    if(rest == nullptr) return cur;
+    if(comparer_(*cur->val_ptr, *rest->val_ptr)) {
+      cur->sibling = rest->child;
+      rest->child = cur;
+      return rest;
+    } else {
+      rest->sibling = cur->child;
+      cur->child = rest;
+      return cur;
+    }
+  }
+
 public:
-	/**
-	 * TODO constructors
-	 */
-	priority_queue() {}
-	priority_queue(const priority_queue &other) {}
-	/**
-	 * TODO deconstructor
-	 */
-	~priority_queue() {}
-	/**
-	 * TODO Assignment operator
-	 */
-	priority_queue &operator=(const priority_queue &other) {}
-	/**
-	 * get the top of the queue.
-	 * @return a reference of the top element.
-	 * throw container_is_empty if empty() returns true;
-	 */
-	const T & top() const {
-
-	}
-	/**
-	 * TODO
-	 * push new element to the priority queue.
-	 */
-	void push(const T &e) {
-
-	}
-	/**
-	 * TODO
-	 * delete the top element.
-	 * throw container_is_empty if empty() returns true;
-	 */
-	void pop() {
-
-	}
-	/**
-	 * return the number of the elements.
-	 */
-	size_t size() const {
-
-	}
-	/**
-	 * check if the container has at least an element.
-	 * @return true if it is empty, false if it has at least an element.
-	 */
-	bool empty() const {
-
-	}
-	/**
-	 * merge two priority_queues with at most O(logn) complexity.
-	 * clear the other priority_queue.
-	 */
-	void merge(priority_queue &other) {
-
-	}
+  priority_queue(): size_(0), root_(nullptr) {}
+  priority_queue(const priority_queue &other): size_(other.size_) {
+    root_ = new Node;
+    root_->val_ptr = new T(*other.root_->val_ptr);
+    copy_heap(root_, other.root_);
+  }
+  priority_queue(priority_queue &&other): root_(other.root_), size_(other.size_) {
+    other.root_ = nullptr;
+    other.size_ = 0;
+  }
+  ~priority_queue() {
+    clear();
+  }
+  priority_queue &operator=(const priority_queue &other) {
+    if(this == &other) return *this;
+    clear();
+    size_ = other.size_;
+    root_ = new Node;
+    root_->val_ptr = new T(*other.root_->val_ptr);
+    copy_heap(root_, other.root_);
+    return *this;
+  }
+  priority_queue &operator=(priority_queue &&other) {
+    if(this == &other) return *this;
+    clear();
+    size_ = other.size_;
+    root_ = other.root_;
+    other.root_ = nullptr;
+    other.size_ = 0;
+    return *this;
+  }
+  const T& top() const {
+    if(empty()) throw container_is_empty();
+    return *root_->val_ptr;
+  }
+  void push(const T &e) {
+    if(empty()) {
+      size_ = 1;
+      Node *node_ptr = new Node;
+      node_ptr->val_ptr = new T(e);
+      root_ = node_ptr;
+      return;
+    }
+    ++size_;
+    if(comparer_(*root_->val_ptr, e)) {
+      Node *node_ptr = new Node;
+      node_ptr->val_ptr = new T(e);
+      node_ptr->child = root_;
+      root_ = node_ptr;
+      return;
+    }
+    Node *node_ptr = new Node;
+    node_ptr->val_ptr = new T(e);
+    node_ptr->sibling = root_->child;
+    root_->child = node_ptr;
+  }
+  void pop() {
+    if(empty()) throw container_is_empty();
+    if(size_ == 1) {
+      clear();
+      return;
+    }
+    Node *cur = root_->child;
+    delete root_->val_ptr;
+    delete root_;
+    root_ = multiple_merge(cur);
+    --size_;
+  }
+  void clear() {
+    free_heap(root_);
+    root_ = nullptr;
+    size_ = 0;
+  }
+  size_t size() const {
+    return size_;
+  }
+  bool empty() const {
+    return size_ == 0;
+  }
+  /**
+   * merge two priority_queues with at most O(logn) complexity.
+   * clear the other priority_queue.
+   */
+  void merge(priority_queue &other) {
+    if(this == &other) {
+      priority_queue another = *this;
+      merge(another);
+      return;
+    }
+    if(other.root_ == nullptr) return;
+    if(root_ == nullptr) {
+      *this = std::move(other);
+      return;
+    }
+    if(comparer_(*root_->val_ptr, *other.root_->val_ptr)) {
+      root_->sibling = other.root_->child;
+      other.root_->child = root_;
+      root_ = nullptr;
+      other.size_ += size_;
+      size_ = 0;
+      *this = std::move(other);
+      return;
+    }
+    other.root_->sibling = root_->child;
+    root_->child = other.root_;
+    other.root_ = nullptr;
+    size_ += other.size_;
+    other.size_ = 0;
+  }
 };
 
 }
